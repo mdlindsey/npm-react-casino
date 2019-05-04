314947563fad5c693d6ce3ab29fee3a1e7cf6ccd
adom <dan@adom.co>
2019-05-04 18:07:31 -0400

    GitHub pages

--- README.md
+++
@@ -1,3 +0,0 @@
-# React Casino Demo
-
-To run this app for development use `npm start`; to build for publishing use `npm run build`. After publishing, commit the `build/` directory to the `gh-pages` branch and push to update the GitHub Pages demo site.
---
+++ precache-manifest.f9a6f686e0c5bb23966d01482711ba97.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fba8f325f79548c59b2d7c24a3dd572",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d62eb9a917230a51dcf",
+    "url": "/static/js/2.cf983c2c.chunk.js"
+  },
+  {
+    "revision": "067cdeaec8617e443270",
+    "url": "/static/js/main.4b37ad1a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "587ebe2e988faf61c3ef760d2572023c",
+    "url": "/static/media/2C.587ebe2e.svg"
+  },
+  {
+    "revision": "2f366a5b72278aea82242332c7183534",
+    "url": "/static/media/2D.2f366a5b.svg"
+  },
+  {
+    "revision": "e0341777d9f62dbf9bed2b7f27957295",
+    "url": "/static/media/2H.e0341777.svg"
+  },
+  {
+    "revision": "23c41a81cc2a26cedd8a3890eec097dc",
+    "url": "/static/media/2S.23c41a81.svg"
+  },
+  {
+    "revision": "bb37256fc94b9d142aec08f72abe2662",
+    "url": "/static/media/3C.bb37256f.svg"
+  },
+  {
+    "revision": "8caae60f1c3dad3b03a270c553334c48",
+    "url": "/static/media/3D.8caae60f.svg"
+  },
+  {
+    "revision": "a7017d69f351b803f02bdbec27990f80",
+    "url": "/static/media/3H.a7017d69.svg"
+  },
+  {
+    "revision": "973ebf000c3040d1a6c58796c3d60463",
+    "url": "/static/media/3S.973ebf00.svg"
+  },
+  {
+    "revision": "9bfa8fb4f5ef394592cb2f91ad2095d3",
+    "url": "/static/media/4C.9bfa8fb4.svg"
+  },
+  {
+    "revision": "0b91458919613eab9d3a94017a9aa913",
+    "url": "/static/media/4D.0b914589.svg"
+  },
+  {
+    "revision": "b5251662aaa21b121b51c5ca07fa2d48",
+    "url": "/static/media/4H.b5251662.svg"
+  },
+  {
+    "revision": "96efb95033fee77e4bbd9f3446a20b76",
+    "url": "/static/media/4S.96efb950.svg"
+  },
+  {
+    "revision": "338868a54e604547d5680849907b4b73",
+    "url": "/static/media/5C.338868a5.svg"
+  },
+  {
+    "revision": "6308d67e0fd65384229c8aab2dd5133e",
+    "url": "/static/media/5D.6308d67e.svg"
+  },
+  {
+    "revision": "2d33a1a0594cb0d0841ba143a505ee6e",
+    "url": "/static/media/5H.2d33a1a0.svg"
+  },
+  {
+    "revision": "7d09239100f12613e54396774580ee70",
+    "url": "/static/media/5S.7d092391.svg"
+  },
+  {
+    "revision": "58b6320be854df77c7c3b53debeddc78",
+    "url": "/static/media/6C.58b6320b.svg"
+  },
+  {
+    "revision": "a0e2355ca3c006e21a2e8da10fac4546",
+    "url": "/static/media/6D.a0e2355c.svg"
+  },
+  {
+    "revision": "08da04ba2a9a6dfb706d326f59367cfe",
+    "url": "/static/media/6H.08da04ba.svg"
+  },
+  {
+    "revision": "67cc9851a689150d0073bd031d0e5ed0",
+    "url": "/static/media/6S.67cc9851.svg"
+  },
+  {
+    "revision": "b36fac40302019a9cd08e192865654c7",
+    "url": "/static/media/7C.b36fac40.svg"
+  },
+  {
+    "revision": "562489c0cea97b73411f5b2734961dfd",
+    "url": "/static/media/7D.562489c0.svg"
+  },
+  {
+    "revision": "a9f215842d486d3f760ebffe5e7a8d8f",
+    "url": "/static/media/7H.a9f21584.svg"
+  },
+  {
+    "revision": "f0172a64cc2989194bd8b274dad61353",
+    "url": "/static/media/7S.f0172a64.svg"
+  },
+  {
+    "revision": "4abf752271696b87a63684c213505ab7",
+    "url": "/static/media/8C.4abf7522.svg"
+  },
+  {
+    "revision": "12470d3d8056e094ed41f362c1b7d27c",
+    "url": "/static/media/8D.12470d3d.svg"
+  },
+  {
+    "revision": "77266c052beb857cf5463742fe209bd8",
+    "url": "/static/media/8H.77266c05.svg"
+  },
+  {
+    "revision": "4c1cf2f549a28968e2e2e794c6057645",
+    "url": "/static/media/8S.4c1cf2f5.svg"
+  },
+  {
+    "revision": "ddc636f526ac81faa31514ae0f8fefff",
+    "url": "/static/media/9C.ddc636f5.svg"
+  },
+  {
+    "revision": "0c5602e72c4c1e724090919878c9ae46",
+    "url": "/static/media/9D.0c5602e7.svg"
+  },
+  {
+    "revision": "3c1e3083f6365d082702b8bdc503d846",
+    "url": "/static/media/9H.3c1e3083.svg"
+  },
+  {
+    "revision": "373f6642bf40720c87a479fff2857da4",
+    "url": "/static/media/9S.373f6642.svg"
+  },
+  {
+    "revision": "2082c029354ca9d20d18885be07ba8fc",
+    "url": "/static/media/AC.2082c029.svg"
+  },
+  {
+    "revision": "8208340f21ceabacb6ade554786c7ae6",
+    "url": "/static/media/AD.8208340f.svg"
+  },
+  {
+    "revision": "5965abb72b03948fdd3bf4ad2235375c",
+    "url": "/static/media/AH.5965abb7.svg"
+  },
+  {
+    "revision": "75abe99feac0e310e15a2fd47bbe5d59",
+    "url": "/static/media/AS.75abe99f.svg"
+  },
+  {
+    "revision": "fccc573d02acf39b2bcd74db5ec75f45",
+    "url": "/static/media/BACK.fccc573d.svg"
+  },
+  {
+    "revision": "e6e64dceb575db77264313e43f850ba9",
+    "url": "/static/media/JC.e6e64dce.svg"
+  },
+  {
+    "revision": "89dee8adcefb381606eb8899c55f8b55",
+    "url": "/static/media/JD.89dee8ad.svg"
+  },
+  {
+    "revision": "9153b20889e6e2b28a2bb3a2448d1254",
+    "url": "/static/media/JH.9153b208.svg"
+  },
+  {
+    "revision": "b5540370a9360b8388865482876d5d99",
+    "url": "/static/media/JS.b5540370.svg"
+  },
+  {
+    "revision": "2d97a850955f61f967ec214cf1110416",
+    "url": "/static/media/KC.2d97a850.svg"
+  },
+  {
+    "revision": "5df7dc65cf0874af51e6f42fbe8bf0e9",
+    "url": "/static/media/KD.5df7dc65.svg"
+  },
+  {
+    "revision": "758fde620e11961ad2db039ffdad0488",
+    "url": "/static/media/KH.758fde62.svg"
+  },
+  {
+    "revision": "82013be946f22983399bfc1617c210a1",
+    "url": "/static/media/KS.82013be9.svg"
+  },
+  {
+    "revision": "9211661c4db9d0ff6bf2beb454d1aace",
+    "url": "/static/media/QC.9211661c.svg"
+  },
+  {
+    "revision": "924c0ab706affda8640bcaf129879d6e",
+    "url": "/static/media/QD.924c0ab7.svg"
+  },
+  {
+    "revision": "77640bb68a71a952af449a210503205b",
+    "url": "/static/media/QH.77640bb6.svg"
+  },
+  {
+    "revision": "627d225b2c312e0c781dbad8d1aaae9c",
+    "url": "/static/media/QS.627d225b.svg"
+  },
+  {
+    "revision": "4eef9bc4feb3b471f61b5e8bd357fce9",
+    "url": "/static/media/TC.4eef9bc4.svg"
+  },
+  {
+    "revision": "3d0ed70f665c9f3c60711cea0cdce94a",
+    "url": "/static/media/TD.3d0ed70f.svg"
+  },
+  {
+    "revision": "5cb0a2f4010df262a6d5338bbacfe448",
+    "url": "/static/media/TH.5cb0a2f4.svg"
+  },
+  {
+    "revision": "dc5647db13b2d3a1f9b617f47cd6802f",
+    "url": "/static/media/TS.dc5647db.svg"
+  }
+]);
